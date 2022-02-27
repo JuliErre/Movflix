b19d71de9a1ebf225b2337e9470658e7e39cde6b
JuliErre <julian.erreguerena@gmail.com>
2022-02-27 17:55:30 -0300

    format

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -23,19 +23,19 @@ function MovieCard({ movie, isLargeImage },) {
   return (
     <div className='flex flex-wrap flex-col max-w-full relative group  '>
       <div className='hover:scale-105 duration-500'>
-      <img onClick={handleClick} className='max-w-sm rounded-md cursor-pointer  ' src={baseUrlImages + (isLargeImage ? movie.poster_path : movie.backdrop_path)} alt="" />
-      <div className='hidden group-hover:flex justify-center items-center left-4 top-4 text-2xl absolute rounded-full w-14 h-14 bg-black/50 border-2 border-white/40 hover:border-white duration-300 group ' >
-        {movieList.find(m => m.id == movie.id) ?
-          <>
-            <FontAwesomeIcon className='text-white cursor-pointer group' icon={faCheck} onClick={() => removeMovie(movie)} />
-          </>
-          :
-          <>
-            <FontAwesomeIcon className='text-white cursor-pointer group relative' icon={faPlus} onClick={() => addToList(movie)} />
+        <img onClick={handleClick} className='max-w-sm rounded-md cursor-pointer  ' src={baseUrlImages + (isLargeImage ? movie.poster_path : movie.backdrop_path)} alt="" />
+        <div className='hidden group-hover:flex justify-center items-center left-4 top-4 text-2xl absolute rounded-full w-14 h-14 bg-black/50 border-2 border-white/40 hover:border-white duration-300 group ' >
+          {movieList.find(m => m.id == movie.id) ?
+            <>
+              <FontAwesomeIcon className='text-white cursor-pointer group' icon={faCheck} onClick={() => removeMovie(movie)} />
+            </>
+            :
+            <>
+              <FontAwesomeIcon className='text-white cursor-pointer group relative' icon={faPlus} onClick={() => addToList(movie)} />
 
-          </>
-        }
-      </div>
+            </>
+          }
+        </div>
       </div>
 
       <div>
