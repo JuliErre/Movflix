416883b3f7a947086dbce9606511d267660185c2
JuliErre <julian.erreguerena@gmail.com>
2022-03-07 16:27:28 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,32 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import HomeScreen from './components/HomeScreen'
-import MovieListContextProvider from './context/MovieListContext'
-import SearchMoviesContextProvider from './context/SearchMoviesContext'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import SearchPage from './components/SearchPage'
-import Navbar from './components/Navbar'
-
-
+import { useState, useContext } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HomeScreen from "./components/HomeScreen";
+import MovieListContextProvider from "./context/MovieListContext";
+import SearchMoviesContextProvider from "./context/SearchMoviesContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
+import Navbar from "./components/Navbar";
+import { MovieListContext } from "./context/MovieListContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { searchText } = useContext(MovieListContext);
 
   return (
-    <div className='bg-neutral-900'  >
-
-      
-        <MovieListContextProvider>
-          <BrowserRouter >
-            <header>
-              <Navbar/>
-            </header>
-          <Routes>
-            <Route exact path="/" element={ <HomeScreen />} />
-            <Route exact path="/search" element={ <SearchPage />} />
-          </Routes>
-         
-          </BrowserRouter>
-        </MovieListContextProvider>
-      
+    <div className="bg-neutral-900">
+      <BrowserRouter>
+        <header>
+          <Navbar />
+        </header>
+        
+        <Routes>
+          <Route exact path="/" element={<HomeScreen />} />
+          <Route exact path="/search/:name" element={<SearchPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
